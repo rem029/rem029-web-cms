26dd99b8b11d5a1e3bfc6f5675588b7b1b4e7640
rem029 <elawrenceponce@gmail.com>
2025-05-05 15:07:51 +0300

    Refactor database adapter configuration for production and development environments in payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -66,24 +66,24 @@ export default buildConfig({
     defaultLocale: 'en',
     locales: ['en', 'ar'],
   },
+
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
   db:
     process.env.NODE_ENV === 'production'
-      ? postgresAdapter({
-          pool: {
-            connectionString: process.env.DATABASE_URI || '',
-          },
-
-          // logger: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
-        })
-      : vercelPostgresAdapter({
+      ? vercelPostgresAdapter({
           pool: {
             connectionString:
               'postgres://postgres.kxsgqmbduwtvxinjscuo:p3D1EoIdY52Plsqq@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x',
           },
+          logger: false,
+        })
+      : postgresAdapter({
+          pool: {
+            connectionString: process.env.DATABASE_URI || '',
+          },
           push: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
-          // logger: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
+          logger: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
         }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
